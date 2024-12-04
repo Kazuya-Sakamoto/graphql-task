c87b34f9d7dc92b77c869ba963a699a34a9d656d
坂本一弥 <sskazuya1130@gmail.com>
2024-12-04 17:58:51 +0900

    create schema.gql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,19 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // コードファースト
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      cors: {
+        origin: '*',
+      },
     }),
+    TaskModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
