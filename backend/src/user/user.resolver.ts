d216996c2213ff8a2ce25fb2020bcd492e5e02ab
坂本一弥 <sskazuya1130@gmail.com>
2024-12-08 12:15:38 +0900

    add: jwt-token

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,9 +1,11 @@
-import { Query, Args, Mutation, Resolver } from '@nestjs/graphql';
-import { UserService } from './user.service';
-import { CreateUserInput } from './dto/createUser.input';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from '@prisma/client';
+import { CreateUserInput } from './dto/createUser.input';
+import { UserService } from './user.service';
 import { User as UserModel } from './models/user.model';
 import { GetUserArgs } from './dto/getUser.args';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Resolver()
 export class UserResolver {
@@ -13,11 +15,12 @@ export class UserResolver {
   async createUser(
     @Args('createUserInput') createUserInput: CreateUserInput,
   ): Promise<User> {
-    return this.userService.createUser(createUserInput);
+    return await this.userService.createUser(createUserInput);
   }
 
   @Query(() => UserModel, { nullable: true })
+  @UseGuards(JwtAuthGuard)
   async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
-    return this.userService.getUser(getUserArgs.email);
+    return await this.userService.getUser(getUserArgs.email);
   }
 }
